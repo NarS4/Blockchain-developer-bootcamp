68d6ef37b6e80ed8232cad93585e8bf9c64ca902
nar <narindra_s@hotmail.com>
2022-12-30 00:52:07 -0500

    31. Transaction status

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ import PriceChart from './PriceChart'
 import Transactions from './Transactions'
 import Trades from './Trades'
 import OrderBook from './OrderBook'
-
+import Alert from './Alert'
 
 function App() {
   const dispatch = useDispatch()
@@ -93,7 +93,7 @@ function App() {
         </section>
       </main>
 
-      {/* Alert */}
+      <Alert />
 
     </div>
   );
