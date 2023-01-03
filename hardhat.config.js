66c71e5f8bc86f43a72fef0858ecc5e69395b062
nar <narindra_s@hotmail.com>
2023-01-02 23:01:05 -0500

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+task("accounts", "Prints list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const accounts of accounts) {
+    console.log(accounts.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost:{}
+    localhost:{}, 
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
   },
 };
